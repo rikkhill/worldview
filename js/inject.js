16437defbf9e5098b978d12f4c4e87167a7b8f43
Rikk Hill <rikk.hill@net-a-porter.com>
2015-06-15 10:22:43 +0100

    Applies clickable bug for real this time

--- js/inject.js
+++ js/inject.js
@@ -1,6 +1,17 @@
+var countries;
+
+function setBug(terms) {
+    terms.forEach(function(c) {
+        var re = new RegExp("(" + c + ")([\\b'.?!,\":;/\)])", 'gi');
+        $("*").replaceText(re, " XX " + '$1$2');
+    });
+}
+
 var json = $.getJSON(chrome.extension.getURL("data/countries.json"),
     function(data){
-        data.countries.forEach(function(c) {
-            $("*").replaceText(c.oldphrase, c.newphrase);
+        var replace = [];
+        $.each(data, function(key, val) {
+            replace.push([val.name]);
         });
+        setBug(replace);
     });
